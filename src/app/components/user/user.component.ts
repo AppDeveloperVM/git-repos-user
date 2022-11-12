8ddd7963492d247472f9a15b4c5348bc17777b79
AppDeveloperVM <vicmr1994@gmail.com>
2022-11-12 20:53:07 +0100

    update user component

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
   error = false;
+  @Input() user = null;
 
   constructor() { }
 
