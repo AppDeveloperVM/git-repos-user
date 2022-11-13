1fb42936c56f63f6f0b6a4b60a96794846b09765
AppDeveloperVM <vicmr1994@gmail.com>
2022-11-13 18:01:38 +0100

    deleted duplicated - models, services - now submodule in use

--- src/app/models/profile.model.ts
+++
@@ -1,7 +0,0 @@
-import { Repo } from "./repo.model";
-import { User } from "./user.model";
-
-export class Profile {
-    user: User;
-    repos: Array<Repo>;
-}
--- src/app/models/repo.model.ts
+++
@@ -1,7 +0,0 @@
-export class Repo {
-    id: number;
-    name : string;
-    html_url: string;
-    created_at: Date;//date
-    updated_at: Date;
-}
