7b1273bdd9f988e0f990c77d2d2bd5892c8a0dbd
AppDeveloperVM <vicmr1994@gmail.com>
2022-11-13 14:28:19 +0100

    updated service githubAPI

--- src/app/services/github-api.service.ts
+++ src/app/services/github-api.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { User } from '../models/user.model';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Repo } from '../models/repo.model';
 
 @Injectable({
   providedIn: 'root'
@@ -13,6 +14,11 @@ export class GithubAPIService {
   getUser(name: string) : Observable<User>{
     const url = `https://api.github.com/users/${name}`;
     return this.http.get<User>(url);
+  }
+
+  getUserRepos(name: string) : Observable<Repo[]>{ 
+    const url = `https://api.github.com/users/${name}/repos`;
+    return this.http.get<Repo[]>(url);
   }
 
 }
